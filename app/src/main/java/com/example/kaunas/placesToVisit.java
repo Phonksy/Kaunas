1e4ae3b99c654de4c4b2c1745b7921745fac693a
Mizoo69 <74656806+Mizoo69@users.noreply.github.com>
2023-04-16 23:08:53 +0300

    Fixed some issues, added landmarks

--- app/src/main/java/com/example/kaunas/placesToVisit.java
+++ app/src/main/java/com/example/kaunas/placesToVisit.java
@@ -26,8 +26,14 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import android.util.Log;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class placesToVisit extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+
 
     private GoogleMap googleMap;
     private MapView mapView;
@@ -37,6 +43,7 @@ public class placesToVisit extends AppCompatActivity implements OnMapReadyCallba
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places_to_visit);
 
@@ -64,10 +71,20 @@ public class placesToVisit extends AppCompatActivity implements OnMapReadyCallba
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         } else {
+            // Get the last known location from the location provider
+            currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            // Move the camera to the last known location
+            if (currentLocation != null) {
+                LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                mapView.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+                    }
+                });
+            }
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         }
-
-
     }
 
     @Override
@@ -146,6 +163,67 @@ public class placesToVisit extends AppCompatActivity implements OnMapReadyCallba
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
 
+        // Add markers for historic landmarks in Kaunas
+        Marker kaunasCastleMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.8953, 23.8863))
+                .title("Kaunas Castle")
+                .snippet("Medieval castle in Kaunas Old Town.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        Marker stMichaelArchangelMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.8959, 23.8901))
+                .title("St. Michael the Archangel Church")
+                .snippet("Historic church in Kaunas Old Town.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        Marker kaunasCathedralMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.8989, 23.8982))
+                .title("Kaunas Cathedral Basilica")
+                .snippet("Catholic cathedral in Kaunas Old Town.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        Marker houseOfPerkunasMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.8977, 23.8907))
+                .title("House of Perkūnas")
+                .snippet("Historic building in Kaunas Old Town.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        Marker vytautasWarMuseumMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.8972, 23.8908))
+                .title("Vytautas the Great War Museum")
+                .snippet("Military museum in Kaunas Old Town.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        Marker ninthFortMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.8971, 23.9011))
+                .title("Ninth Fort Museum")
+                .snippet("Museum and memorial to the victims of Nazi and Soviet terror.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        Marker pazaislisMonasteryMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.9343, 23.9463))
+                .title("Pazaislis Monastery")
+                .snippet("Baroque monastery on the outskirts of Kaunas.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        Marker kaunasTownHallMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.8982, 23.9044))
+                .title("Kaunas Town Hall")
+                .snippet("Historic town hall in Kaunas Old Town.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        Marker kaunasGhettoMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.9044, 23.8938))
+                .title("Kaunas Ghetto")
+                .snippet("Site of the wartime Jewish ghetto in Kaunas.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        Marker christsResurrectionMarker = googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.9009, 23.8935))
+                .title("Christ's Resurrection Church")
+                .snippet("Orthodox church in Kaunas Old Town.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
         // Enable the zoom controls on the map
         googleMap.getUiSettings().setZoomControlsEnabled(true);
 
@@ -158,49 +236,21 @@ public class placesToVisit extends AppCompatActivity implements OnMapReadyCallba
             // Move the camera to the user's current location
             if (currentLocation != null) {
                 LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+            }
+            else {
+                LatLng kaunasLatLng = new LatLng(54.8985, 23.9036);
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kaunasLatLng, 5));
             }
         } else {
             // Request location permission if not granted
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-
-        // Add markers for the places to visit in Kaunas
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(54.901653, 23.899986))
-                .title("Kaunas Castle")
-                .snippet("Historic castle and museum"));
-
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(54.898738, 23.909745))
-                .title("Town Hall Square")
-                .snippet("Historic town square with cafes and restaurants"));
-
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(54.899553, 23.895819))
-                .title("Vytautas the Great War Museum")
-                .snippet("Military history museum"));
-
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(54.898226, 23.917162))
-                .title("St. Michael the Archangel Church")
-                .snippet("Baroque church with frescoes and sculptures"));
-
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(54.899799, 23.912330))
-                .title("Kaunas Cathedral Basilica")
-                .snippet("Neoclassical cathedral with a bell tower"));
-
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(54.898076, 23.919898))
-                .title("Perkunas House")
-                .snippet("Historic Gothic building with exhibitions"));
     }
+
     @Override
     public void onLocationChanged(Location location) {
-        currentLocation = location;
-        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        googleMap.addMarker(new MarkerOptions().position(latLng).title("You are here"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
+        // Update the current location
+        //currentLocation = location;
     }
 }
